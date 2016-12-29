51b2d9e7d01768eee7e8cd745c3d38488091579e
Viet <viettangle12@gmail.com>
2016-12-29 01:34:36 -0500

    hashing url

---
+++ src/main/java/com/le/viet/vault/controller/VaultController.java
@@ -0,0 +1,14 @@
+package com.le.viet.vault.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rs")
+public class VaultController {
+	@RequestMapping(value = "/ping", method = RequestMethod.GET)
+	public String ping(){
+		return "pong";
+	}
+}
